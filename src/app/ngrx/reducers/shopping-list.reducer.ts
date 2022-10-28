c3729a1cef8b0dfdc88567bf4490d19ef3494d8d
EstebanNajera <esteban_fernando@live.com.mx>
2022-10-28 15:47:51 -0500

    Se integra shopping-list reducer
    Se integra el primer reducer sin funcionalidad aun al proyecto

---
+++ src/app/ngrx/reducers/shopping-list.reducer.ts
@@ -0,0 +1,20 @@
+import { Action } from "@ngrx/store";
+import { Ingredient } from "src/app/models/ingredient.model";
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Tomatoes', 5),
+    new Ingredient('Potatoes', 15)
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case value:
+
+      break;
+
+    default:
+      break;
+  }
+}
