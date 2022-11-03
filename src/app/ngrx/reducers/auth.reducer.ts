5ad7b3a6ee5125da9bd7e0324339ddab0cdf9f69
EstebanNajera <esteban_fernando@live.com.mx>
2022-11-02 21:58:35 -0600

    Auth Reducer y Actions
    - Se integran las acciones que ejecutaran y se integran clases Action para el uso de acciones
    - Se integran las acciones Auth al reducer

--- src/app/ngrx/reducers/auth.reducer.ts
+++ src/app/ngrx/reducers/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from "src/app/models/user.model"
+import * as AuthActions  from "../actions/auth.actions";
 
 export interface State {
   user: User;
@@ -8,6 +9,21 @@ const initialState = {
   user: null
 }
 
-export function authReducer(state: State = initialState, action) {
-  return state
+export function authReducer(state: State = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const payload = action.payload;
+      const newUser: User = new User(payload.email, payload.userId, payload.token, payload.expirationDate);
+      return {
+        ...state,
+        user: newUser
+      };
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null
+      };
+    default:
+      return state;
+  }
 }
