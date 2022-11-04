2ab296121c5d77554500661b360db2f8e6ab7f9d
EstebanNajera <esteban_fernando@live.com.mx>
2022-11-03 19:15:21 -0600

    Login start effect
    Se integra un nuevo effect para poder interactuar con el action LOGIN_START, esto nos permitira poder realizar la consulta http desde ngrx effect

--- src/app/ngrx/effects/auth.effects.ts
+++ src/app/ngrx/effects/auth.effects.ts
@@ -1,12 +1,42 @@
-import { Actions, ofType } from "@ngrx/effects";
+import { HttpClient } from "@angular/common/http";
+import { Actions, ofType, Effect } from "@ngrx/effects";
+import { catchError, map, of, switchMap } from "rxjs";
+import { environment } from "src/environments/environment";
 import * as AuthActions from '../actions/auth.actions';
 
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
 export class AuthEffects {
-  authLogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START)
+  url = 'https://identitytoolkit.googleapis.com/v1/accounts';
+  @Effect() authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      const body = {
+        email: authData.payload.email,
+        password: authData.payload.password,
+        returnSecureToken: true
+      };
+      return this.http.post<AuthResponseData>
+        (`${this.url}:signInWithPassword?key=${environment.firebaseAPIKey}`, body)
+        .pipe(catchError((error) => {
+          of();
+        }), map(resData => {
+          of();
+        }));
+    }),
+
   );
 
   constructor(
+    private http: HttpClient,
     private actions$: Actions
   ) {}
 }
